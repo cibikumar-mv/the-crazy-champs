4fd839c621d1a584334190725f018860548fda17
Hareesh Raj <hareesh1705@gmail.com>
2021-03-14 13:23:16 +0530

    the comment for subtraction

--- subtraction/subtraction.py
+++ subtraction/subtraction.py
@@ -1,5 +1,10 @@
 def do_subtraction(x,y):
+    '''
+    the two values are recieved
+    subtraction has been done
+    the value has been returned
+    '''
     val=x-y
     return val
 res=do_subtraction(10,5)
-print(res)
+print("The subtracted value is",res)
